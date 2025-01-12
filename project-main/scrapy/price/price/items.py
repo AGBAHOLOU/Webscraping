03ce98bdbb99fcf9fa340e8c8a25360375dc373b
Salwane <salwanealao63@gmail.com>
2025-01-12 15:26:42 +0100

    Ajout ou mise à jour des fichiers nécessaires

--- project-main/scrapy/price/price/items.py
+++ project-main/scrapy/price/price/items.py
@@ -12,7 +12,9 @@ class ProjetItem(scrapy.Item):
     pass
 
 class GameItem(scrapy.Item):
+    image = scrapy.Field()
     name = scrapy.Field()
+    category =scrapy.Field()
     price = scrapy.Field()
-    url = scrapy.Field()
     site = scrapy.Field()
+    url = scrapy.Field()
